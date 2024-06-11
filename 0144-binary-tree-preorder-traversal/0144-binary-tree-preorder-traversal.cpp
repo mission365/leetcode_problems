6cb1fc32963b281377b646f68dab7d6e6cda2f7b
Mission Devnath <117886802+mission365@users.noreply.github.com>
2024-06-11 13:04:49 +0600

    Time: 0 ms (100.00%), Space: 9.9 MB (19.44%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,18 +11,16 @@
  */
 class Solution {
 public:
-
-    void preorder(TreeNode* root, vector<int> &ans) {
-        if(root != NULL) {
-            ans.push_back(root->val);
-            preorder(root->left, ans);
-            preorder(root->right, ans);
-        }
+    void preOrder(TreeNode *root, vector<int> &v){
+        if(root==NULL) return;
+        v.push_back(root->val);
+        preOrder(root->left,v);
+        preOrder(root->right,v);
     }
-
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        preorder(root,ans);
-        return ans;
+        // if(root==NULL) return;
+        vector<int> v;
+        preOrder(root,v);
+        return v;
     }
 };
