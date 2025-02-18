0544c459e9f6a8c170ee3fadd45147cd66b5decc
Mission Devnath <117886802+mission365@users.noreply.github.com>
2025-02-18 18:44:11 +0600

    Time: 0 ms (100%), Space: 9.1 MB (57.83%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int n = s.size();
+        int index = 0;
+        while(index<n && s[index]==' '){
+            index++;
+        }
+        bool pos = true; int isfound=0;
+        if(index<n && (s[index]=='+' || s[index]=='-')){
+            pos = (s[index]=='+');
+            index++;
+        }
+        
+        long long result = 0;
+
+        for(int i=index;i<n;i++){
+            if(s[i]>='0' && s[i]<='9'){
+                result = result*10 + (s[i]-'0');
+                if(result>INT_MAX){
+                    return pos?INT_MAX:INT_MIN;
+                }
+            }
+            else break;
+        }
+        return pos ? result : -result;
+    }
+};
