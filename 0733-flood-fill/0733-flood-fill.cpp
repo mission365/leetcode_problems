d545e043515d0257ce6d2e6b1331fe00a3ad7cd7
Mission Devnath <117886802+mission365@users.noreply.github.com>
2025-02-23 22:32:29 +0600

    Time: 1 ms (23.47%), Space: 18.5 MB (17.92%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,37 +1,73 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+
+    void dfs(int r,int c, vector<vector<int>> image, vector<vector<int>> &ans, 
+    int color, int inival, int delrow[],int delcol[]) {
+        ans[r][c] = color;
         int n = image.size();
         int m = image[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        queue<pair<int,int>> q;
-        vis[sr][sc] = 1;
-        q.push({sr,sc});
-        int ini = image[sr][sc];
+
+        for(int i=0;i<4;i++){
+            int nrow = r+delrow[i];
+            int ncol = c+delcol[i];
+
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol] == inival && ans[nrow][ncol] != color){
+                dfs(nrow,ncol,image, ans, color, inival,delrow,delcol);
+            }
+        }
+    }
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+
         
+        int inival = image[sr][sc]; 
         vector<vector<int>> ans = image;
 
         int delrow[] = {-1,0,1,0};
         int delcol[] = {0,1,0,-1};
 
-        while(!q.empty()){
-            int r = q.front().first;
-            int c = q.front().second;
-            q.pop();
-            // ans.push_back(image[r][c]);
-            ans[r][c] = color;
-            for(int i=0;i<4;i++){
-                int nrow = r+delrow[i];
-                int ncol = c+delcol[i];
-
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol] == ini && 
-                !vis[nrow][ncol]){
-                    // ans[nrow][ncol] = color;
-                    vis[nrow][ncol] = 1;
-                    q.push({nrow,ncol});
-                }
-            }
-        }
+        dfs(sr,sc,image, ans, color, inival,delrow, delcol);
         return ans;
     }
 };
+
+
+//BFS solution:
+
+// class Solution {
+// public:
+//     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+//         int n = image.size();
+//         int m = image[0].size();
+//         vector<vector<int>> vis(n,vector<int>(m,0));
+//         queue<pair<int,int>> q;
+//         vis[sr][sc] = 1;
+//         q.push({sr,sc});
+//         int ini = image[sr][sc];
+        
+//         vector<vector<int>> ans = image;
+
+//         int delrow[] = {-1,0,1,0};
+//         int delcol[] = {0,1,0,-1};
+
+//         while(!q.empty()){
+//             int r = q.front().first;
+//             int c = q.front().second;
+//             q.pop();
+//             // ans.push_back(image[r][c]);
+//             ans[r][c] = color;
+//             for(int i=0;i<4;i++){
+//                 int nrow = r+delrow[i];
+//                 int ncol = c+delcol[i];
+
+//                 if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol] == ini && 
+//                 !vis[nrow][ncol]){
+//                     // ans[nrow][ncol] = color;
+//                     vis[nrow][ncol] = 1;
+//                     q.push({nrow,ncol});
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
