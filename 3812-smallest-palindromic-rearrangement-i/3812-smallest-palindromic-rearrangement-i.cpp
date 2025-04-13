51c7892ab6cbdb12cbeac5fef080aeda2d2dbb7c
Mission Devnath <117886802+mission365@users.noreply.github.com>
2025-04-13 11:50:03 +0600

    Time: 150 ms (26.32%), Space: 71.6 MB (63.16%) - LeetHub

--- 3812-smallest-palindromic-rearrangement-i/3812-smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/3812-smallest-palindromic-rearrangement-i.cpp
@@ -2,35 +2,13 @@ class Solution {
 public:
     string smallestPalindrome(string s) {
         int n = s.size();
-        if(n%2==0){
-            string first = "";
-        for(int i=0;i<n/2;i++){
-            first+=s[i];
-        }
-        string second = "";
-        for(int i=n/2;i<n;i++){
-            second+=s[i];
-        }
+        string first = s.substr(0,n/2);
         sort(first.begin(),first.end());
-        sort(second.rbegin(),second.rend());
-        return first+second;
-        }
-        else{
-            string first = "";
-        for(int i=0;i<n/2;i++){
-            first+=s[i];
-        }
-        string second = "";
-        for(int i=n/2+1;i<n;i++){
-            second+=s[i];
-        }
-        sort(first.begin(),first.end());
-        sort(second.rbegin(),second.rend());
-        string ans = "";
-        ans+=first;
-        ans+=s[n/2];
-        ans+=second;
-        return ans;
-        }
+
+        string last = first;
+        sort(last.rbegin(),last.rend());
+
+        if(n%2==1) first += s[n/2];
+        return first+last;
     }
 };
