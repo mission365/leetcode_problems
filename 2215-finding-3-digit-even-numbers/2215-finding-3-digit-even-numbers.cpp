3e72fc56496b65ed7b23674615e23498defd5a0a
Mission Devnath <117886802+mission365@users.noreply.github.com>
2025-05-12 23:12:21 +0600

    Time: 3 ms (81.83%), Space: 12.2 MB (99.4%) - LeetHub

---
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.cpp
@@ -0,0 +1,28 @@
+
+class Solution {
+public:
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        vector<int> mp(10,0);
+        for(int i=0;i<digits.size();i++){
+            mp[digits[i]]++;
+        }
+        vector<int> ans;
+        for(int i=1;i<=9;i++){
+            if(mp[i]==0) continue;
+            mp[i]--;
+
+            for(int j=0;j<=9;j++){
+                if(mp[j]==0) continue;
+                mp[j]--;
+
+                for(int k=0;k<=8;k+=2){
+                    if(mp[k]==0) continue;
+                    ans.push_back(i*100 + j*10 + k);
+                }
+                mp[j]++;
+            }
+            mp[i]++;
+        }
+        return ans;
+    }
+};
