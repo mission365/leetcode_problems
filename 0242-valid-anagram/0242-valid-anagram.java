ca6180da109aa2fa796917193b4af6c2176de5c7
Mission Devnath <117886802+mission365@users.noreply.github.com>
2025-06-01 19:08:54 +0600

    Time: 27 ms (5.34%), Space: 44.8 MB (25.9%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        Map<Character, Integer> smp = new HashMap<>();
+        Map<Character, Integer> tmp = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            smp.put(s.charAt(i), smp.getOrDefault(s.charAt(i), 0) + 1);
+            tmp.put(t.charAt(i), tmp.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        return smp.equals(tmp);
+    }
+}
