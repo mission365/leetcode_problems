de5053a7381aa87f806980c1660f017022d9af2b
Mission Devnath <117886802+mission365@users.noreply.github.com>
2025-07-09 00:48:14 +0600

    Time: 0 ms (100%), Space: 13.5 MB (5.73%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,15 +11,17 @@
  */
 class Solution {
 public:
-    void inOrder(TreeNode *root, vector<int> &v){
-        if(root == NULL) return;
-        inOrder(root->left,v);
-        v.push_back(root->val);
-        inOrder(root->right,v);
+    vector<int> inOrder(TreeNode* root, vector<int> &ans){
+        if(root != NULL){
+            inOrder(root->left, ans);
+            ans.push_back(root->val);
+            inOrder(root->right,ans);
+        }
+        return ans;
     }
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> v;
-        inOrder(root, v);
-        return v;
+        vector<int> ans;
+        inOrder(root, ans);
+        return ans;
     }
 };
